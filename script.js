a9ea76ab5e72cfe927065889e9a869b3638e1fec
OldWhaler <76100109+OldWhaler@users.noreply.github.com>
2022-07-23 21:22:34 +0300

    feat: add solution to lesson-03

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 "use strict"
 
-const money = 43000;
 const profit = 'freelance';
-const expenses = 'Проезд, Питание, Развлечения, Отпуск';
 const purpose = 80000;
 const period = 4;
-const budgetDay = Math.floor(money / 30);
+const money = prompt(`Ваш месячный доход?`);
+const expenses = prompt(`Перечислите возможные расходы за рассчитываемый период через запятую`);
+const amount = prompt(`Во сколько обойдутся обязательные статьи расходов?`);
+const deposit = confirm(`Есть ли у вас вклад в банке?`);
+const budgetMonth = money - amount;
+const budgetDay = Math.floor(budgetMonth / 30);
 
 console.log(`Тип данных переменной money: ${typeof money}.`);
 console.log(`Тип данных переменной profit: ${typeof profit}.`);
 console.log(`Длинна строки expenses: ${expenses.length}.`);
 console.log(`Период равен ${period} месяцев.`);
 console.log(`Цель заработать ${purpose} рублей.`);
-console.log(`Дневной бюджет ${budgetDay} рублей.`);
+console.log(`Месячный бюджет ${budgetMonth} рублей.`);
+console.log(`Дневной бюджет ${Math.floor(budgetDay)} рублей.`);
+console.log(`Цель будет достигнута через ${Math.ceil(purpose / budgetMonth)} месяцев.`)
+
+if (budgetDay > 60000) {
+  console.log(`У вас высокий уровень дохода`)
+} else if (budgetDay > 30000 && budgetDay <= 60000) {
+  console.log(`У вас средний уровень дохода`)
+} else if (budgetDay <= 30000 && budgetDay > 0) {
+  console.log(`К сожалению у вас уровень дохода ниже среднего`)
+} else {
+  console.log(`Что то пошло не так`)
+}
+
 
 const expensesArr = expenses.toLocaleLowerCase().match(/[а-яА-ЯёЁ]+/ig);
 console.log(expensesArr)
+
+
+
