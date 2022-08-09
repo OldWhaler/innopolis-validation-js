695781e1314ca8a9101044e56fddb11170d047df
OldWhaler <76100109+OldWhaler@users.noreply.github.com>
2022-08-09 18:32:42 +0300

    feat: add solution to lesson-06

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 "use strict"
 
-const profit = 'freelance';
-const purpose = 80000;
-const period = 4;
-const expenses = prompt(`Перечислите возможные расходы за рассчитываемый период через запятую`);
+// const profit = 'freelance';
+// const purpose = 80000;
+// const period = 4;
+// const expenses = prompt(`Перечислите возможные расходы за рассчитываемый период через запятую`);
 
-let money;
-while (true) {
-  money = +prompt(`Ваш месячный доход?`);
-  if (isFinite(money)) break;
-  alert('Введите число');
-}
+// let money;
+// while (true) {
+//   money = +prompt(`Ваш месячный доход?`);
+//   if (isFinite(money)) break;
+//   alert('Введите число');
+// }
 
-let extraMoney;
-while (true) {
-  extraMoney = +prompt(`Перечислите возможный доход за ваши дополнительные работы: ${profit}?`);
-  if (isFinite(extraMoney)) break;
-  alert('Введите число');
-}
+// let extraMoney;
+// while (true) {
+//   extraMoney = +prompt(`Перечислите возможный доход за ваши дополнительные работы: ${profit}?`);
+//   if (isFinite(extraMoney)) break;
+//   alert('Введите число');
+// }
 
-let amount;
-while (true) {
-  amount = +prompt(`Во сколько обойдутся обязательные статьи расходов?`);
-  if (isFinite(amount)) break;
-  alert('Введите число');
-}
+// let amount;
+// while (true) {
+//   amount = +prompt(`Во сколько обойдутся обязательные статьи расходов?`);
+//   if (isFinite(amount)) break;
+//   alert('Введите число');
+// }
 
-const deposit = confirm(`Есть ли у вас вклад в банке?`);
-const accumulatedIncome = getAccumulatedIncome();
-const budgetDay = Math.floor(accumulatedIncome / 30);
+// const deposit = confirm(`Есть ли у вас вклад в банке?`);
+// const accumulatedIncome = getAccumulatedIncome();
+// const budgetDay = Math.floor(accumulatedIncome / 30);
 
-console.log('Ваш бюджет на месяц с учетом ваших расходов составляет: ', getAccumulatedIncome());
-console.log('Дневной бюджет', budgetDay);
+// console.log('Ваш бюджет на месяц с учетом ваших расходов составляет: ', getAccumulatedIncome());
+// console.log('Дневной бюджет', budgetDay);
 
-if (getTargetMonth() > 0) {
-  console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов будет достигнута через`, getTargetMonth() + ' месяца');
-} else {
-  console.log('Цель не будет достигнута');
-}
+// if (getTargetMonth() > 0) {
+//   console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов будет достигнута через`, getTargetMonth() + ' месяца');
+// } else {
+//   console.log('Цель не будет достигнута');
+// }
 
-function getAccumulatedIncome() {
-  return money - amount + extraMoney;
-};
-function getTargetMonth() {
-  return Math.ceil(purpose / accumulatedIncome);
-};
+// function getAccumulatedIncome() {
+//   return money - amount + extraMoney;
+// };
+// function getTargetMonth() {
+//   return Math.ceil(purpose / accumulatedIncome);
+// };
 
 /* if (budgetDay > 60000) {
   console.log(`У вас высокий уровень дохода`)
@@ -61,5 +61,31 @@ function getTargetMonth() {
 console.log(expensesArr) */
 
 
+async function getUsersInfo() {
+  const res = await fetch('https://reqres.in/api/users?per_page=12');
+  const users = await res.json();
+  console.log('-----------');
+  console.log('Пункт №1:')
+  console.log('-----------');
+  users.data.forEach(user => console.log(user.last_name))
+  console.log('-----------');
+  console.log('Пункт №2:')
+  console.log('-----------');
+  users.data.forEach(user => {
+    if (user.last_name[0] === 'F') console.log(user)
+  })
+  console.log('-----------');
+  console.log('Пункт №3:')
+  console.log('-----------');
+  const task3 = users.data.reduce((acc, cur) => {
+    return acc += ` ${cur.first_name} ${cur.last_name},`
+  }, 'Наша база содержит данные следующих пользователей:')
+  console.log(task3.slice(0, -1))
+  console.log('-----------');
+  console.log('Пункт №4:')
+  console.log('-----------');
+  console.log(Object.keys(users.data[0]).toString())
+}
 
+getUsersInfo()
 
